4e50be6258e6d34c8575379b69bdfa37d3323db5
LambFerret <dignrjrl12@gmail.com>
2021-07-12 23:25:49 +0900

    20210712

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,36 +4,88 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    public float jump;
-    public float maxSpeed;
-
     public GameObject NeogulMan;
+    public float jump;
+    public float speed;
+    bool isJumping = false;
     Rigidbody2D rb;
+    Animator ani;
+    Vector3 playerDir;
     // Start is called before the first frame update
     void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-        float h = Input.GetAxisRaw("Horizontal");
-
-        rb.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-        if (rb.velocity.x > maxSpeed)
-            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
-        else if (rb.velocity.x < maxSpeed * (-1)) 
-            rb.velocity = new Vector2(maxSpeed * (-1), rb.velocity.y);
-
-
-
-
         if (Input.GetKeyDown(KeyCode.X))
         {
-            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-        } 
+            isJumping = true;
+        }
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+    }
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if (h > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            playerDir = Vector3.right;
+        }
+        else if (h < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            playerDir = Vector3.left;
+
+        }
+        rb.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        if (rb.velocity.x > speed)
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        else if (rb.velocity.x < speed * (-1))
+            rb.velocity = new Vector2(speed * (-1), rb.velocity.y);
+
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+            return;
+        if (playerDir == Vector3.left)
+        {
+            Vector2 jumpVelocity = new Vector2(-jump, jump * 2);
+            rb.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        }
+        else
+        {
+            Vector2 jumpVelocity = new Vector2(jump, jump * 2);
+            rb.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        }
+        isJumping = false;
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("attach" + other.gameObject.layer);
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("detach" + other.gameObject.layer);
     }
 }
